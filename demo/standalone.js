1b252c7515e5ce0331b77204b83c9f2d0bb4112d
Tom Giratikanon <tom.giratikanon@nytimes.com>
2015-08-25 19:40:42 -0400

    Update standalone.js

--- demo/standalone.js
+++ demo/standalone.js
@@ -1,8 +1,10 @@
 import {ProseMirror} from "../src/edit/main"
-import {Pos, Node} from "../src/model"
+import {Pos, Node, rangeHasStyle, style} from "../src/model"
 import {fromDOM} from "../src/convert/from_dom"
 
 window.ProseMirror = ProseMirror
 window.Pos = Pos
 window.Node = Node
 window.fromDOM = fromDOM
+window.rangeHasStyle = rangeHasStyle
+window.style = style
